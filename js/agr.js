0a5d9f316f91a2b8304f28b5eb827ee7d1a07632
seldonplan2 <89587110+seldonplan2@users.noreply.github.com>
2021-08-29 08:05:51 +0900

    定期の修正
    8/28の感染者データを追加
    回数別ワクチン接種率の分布グラフを追加
    その他表記の調整

--- js/agr.js
+++ js/agr.js
@@ -62,6 +62,13 @@ let tokyoDomPostCaseObj = {
  * 東京-全国 感染者数比較グラフ
  */
 function tokyoDomPostCaseGraph() {
+
+    //東京/全国のデータを呼び出し
+    tokyoDomPostCaseObj.rate_data.data.forEach(function(data, index) {
+        //%表記用に100倍にする
+        tokyoDomPostCaseObj.rate_data.data[index] = Math.trunc(data * 100);
+    });
+
     //チャートデータ
     let c_data = {
         labels: tokyoDomPostCaseObj.horizontal,
@@ -81,11 +88,11 @@ function tokyoDomPostCaseGraph() {
             //東京
             {
                 type: 'bar',
-                label: tokyoDomPostCaseObj.tokyo_data.label,
                 backgroundColor: agrChartColors.red,
-                data: tokyoDomPostCaseObj.tokyo_data.data,
                 borderColor: agrChartColors.red,
                 borderWidth: 2,
+                label: tokyoDomPostCaseObj.tokyo_data.label,
+                data: tokyoDomPostCaseObj.tokyo_data.data,
                 yAxisID: 'y-axis-1',//命名は"y-axis-連番"でないといけない(みたい
             },
             //全国
@@ -119,7 +126,18 @@ function tokyoDomPostCaseGraph() {
             //複数表示に必要？
             tooltips: {
                 mode: 'index',
-                intersect: true
+                intersect: true,
+                callbacks: {
+                    //ラベル
+                    label: function (tooltipItem, data) {
+                        let tooltip = data.datasets[tooltipItem.datasetIndex];
+                        if(tooltipItem.datasetIndex === 0){
+                            return tooltip.label + ': ' + tooltipItem.value + '%';
+                        }else{
+                            return tooltip.label + ': ' + parseInt(tooltipItem.value).toLocaleString() + '人';
+                        }
+                    },
+                }
             },
             //ラインを曲線に"しない"指定
             elements: {
@@ -264,7 +282,14 @@ function spPrefDomPostCaseGraph() {
             //複数表示に必要？
             tooltips: {
                 mode: 'index',
-                intersect: true
+                intersect: true,
+                callbacks: {
+                    //ラベル
+                    label: function (tooltipItem, data) {
+                        let tooltip = data.datasets[tooltipItem.datasetIndex];
+                        return tooltip.label + ': ' + parseInt(tooltipItem.value).toLocaleString() + '人';
+                    },
+                }
             },
             //ラインを曲線に"しない"指定
             elements: {
@@ -276,6 +301,93 @@ function spPrefDomPostCaseGraph() {
                 point: {
                     radius: Radius,
                     hitRadius: HitRadius,
+                }
+            },
+        }
+    });
+}
+
+/**
+ * 主要都道府県・地域 感染者数推移グラフ
+ */
+function vaccinePostCaseRateGraph(title, data) {
+
+    //チャートデータ
+    let c_data = {
+        datasets: data,
+    };
+
+    //初期化
+    let r, color;
+    //表データセット呼び出し
+    c_data.datasets.forEach(function(dataset, index) {
+        //
+        r = dataset.data[0]['r'];
+        //
+        if(1.0 <= r) {
+            color = agrChartColors.red;
+        }else if(0.5 <= r){
+            color = agrChartColors.lightpink;
+        }else if(0.1 <= r){
+            color = agrChartColors.orange;
+        }else{
+            color = agrChartColors.blue;
+        }
+        c_data.datasets[index].backgroundColor = color;
+
+        c_data.datasets[index].data[0]['r'] = r * 20;
+        c_data.datasets[index].original_radius = r;
+        c_data.datasets[index].sum = c_data.datasets[index].sum.toLocaleString();
+    });
+
+    //要素を取得
+    let element = document.getElementById('vaccinePostCaseRateGraph');
+    //高さを設定
+    element.height = 180;
+    //描画機能を有効化
+    let ctx2 = element.getContext('2d');
+    //グラフを描画
+    window.total = new Chart(ctx2, {
+        type: 'bubble',
+        data: c_data,
+        options: {
+            title: {
+                text: title,
+                display: true
+            },
+            //凡例
+            legend: {
+                display: false,
+            },
+            // スケール
+            scales: {
+                // x軸
+                xAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: '1回目',
+                        fontColor: 'black',
+                        fontSize: 16,
+                    },
+                }],
+                // x軸
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: '2回目',
+                        fontColor: 'black',
+                        fontSize: 16,
+                    },
+                }]
+            },
+            tooltips: {
+                callbacks: {
+                    //ラベル
+                    label: function (tooltipItem, data) {
+                        //ツールチップを取得
+                        let tooltip = data.datasets[tooltipItem.datasetIndex];
+                        return tooltip.label + ' 感染者: ' + tooltip.original_radius + '% (' + tooltip.sum + '人)';
+                    },
                 }
             },
         }
